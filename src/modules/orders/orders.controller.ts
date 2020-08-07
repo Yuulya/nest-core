598ed926d6deee47610200c7e2f2915dddb7fede
Phi Nguyen <phi.nguyen@sotatek.com>
2020-08-07 15:53:32 +0700

    feat: modify a bit

--- src/modules/orders/orders.controller.ts
+++ src/modules/orders/orders.controller.ts
@@ -1,8 +1,23 @@
-import {Controller} from '@nestjs/common';
+import {Controller, Post, Get} from '@nestjs/common';
 import {ApiTags} from '@nestjs/swagger';
 
 @Controller('orders')
 @ApiTags('orders')
 export class OrderController {
+  @Post('create_order')
+  createOrder() {
+    // mark order status as created
+    return {};
+  }
 
+  @Post('cancel_order')
+  cancelOrder() {
+    // mark order status as canceled
+    return {}
+  }
+
+  @Get('get_order')
+  getOrder() {
+    return {}
+  }
 }
